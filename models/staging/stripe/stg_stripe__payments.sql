bae7ab12be3aa1ae45eed28b8460da44599c11df
Ram Godavarthi <ramkiran.g@gmail.com>
2026-02-04 17:36:34 -0800

    finished fundamentals demo

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,10 @@
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        amount / 100 as amount,
+        created,
+        _batched_at
+
+    from {{ source('stripe','payment')}}
